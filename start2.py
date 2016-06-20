4f1ca8fb7191add2e72071f2bfde14d1244d0af9
Shaunak Sen <shaunak1105@gmail.com>
2016-06-20 22:55:12 +0530

    assignment done!

---
+++ start2.py
@@ -0,0 +1,14 @@
+import socket
+
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysocket.connect(('www.py4inf.com', 80))
+
+mysocket.send('GET http://py4inf.com/code/romeo.txt HTTP/1.0\n\n')
+
+while True:
+    data = mysocket.recv(512)
+    if len(data) < 1:
+        break
+    print data
+
+mysocket.close()
