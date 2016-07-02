dbe08683e04abeffdcd3f83519033118b38f84de
Shaunak Sen <shaunak1105@gmail.com>
2016-07-02 13:52:16 +0530

    completed itunes project

--- access_db/start3.py
+++ access_db/start3.py
@@ -1,6 +1,30 @@
 import xml.etree.ElementTree as ET
 import sqlite3
 
+conn = sqlite3.connect('trackdb.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Artist (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name TEXT UNIQUE
+)''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Album (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    artist_id INTEGER,
+    title TEXT UNIQUE
+)''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Track (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title TEXT UNIQUE,
+    album_id INTEGER,
+    len INTEGER, rating INTEGER, count INTEGER
+)''')
+
 fname = 'tracks/Library.xml'
 
 
@@ -13,6 +37,7 @@ def lookup(d, key):
             found = True
     return None
 
+
 stuff = ET.parse(fname)
 
 all = stuff.findall('dict/dict/dict')
@@ -20,4 +45,40 @@ all = stuff.findall('dict/dict/dict')
 print 'Dict count:', len(all)
 
 for entry in all:
-    lookup(entry, 'Name')
+    if lookup(entry, 'Track ID') is None:
+        continue
+
+    name = lookup(entry, 'Name')
+    artist = lookup(entry, 'Artist')
+    album = lookup(entry, 'Album')
+    count = lookup(entry, 'Play Count')
+    rating = lookup(entry, 'Rating')
+    length = lookup(entry, 'Total Time')
+
+    if name is None or album is None or artist is None:
+        continue
+
+    print name, artist, album, count, rating, length
+
+    cur.execute('''
+    INSERT OR IGNORE INTO Artist(name)
+    VALUES ( ? )
+    ''', (artist, ))
+
+    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute('''
+    INSERT OR IGNORE INTO Album (title, artist_id)
+    VALUES ( ?, ? )
+    ''', (album, artist_id))
+
+    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''
+    INSERT OR REPLACE INTO Track (title, album_id, len, rating, count)
+    VALUES ( ?, ?, ?, ?, ? )
+    ''', (name, album_id, length, rating, count))
+
+    conn.commit()
